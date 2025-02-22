d126896394d995efb35168de5b170044658cdfa9
janai <janai@JANAIDSHAIK>
2025-02-22 13:07:52 +0200

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //  Get /HelloWorld/
+        public string index()
+        {
+            return "This is my default action";
+        }
+
+        //  Get /HelloWorld/Welcome
+        public string Welcome()
+        {
+            return "This  is the Welcome action needed";
+        }
+    }
+}
